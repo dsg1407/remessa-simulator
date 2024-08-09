a7a3f747e8031e711549f54210e543ec27c395e1
Daniel S Goncalves <dsg1407@gmail.com>
2024-08-09 11:53:35 -0300

    Add Google Third-Party

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Poppins, Nunito } from 'next/font/google'
 import './globals.css'
 import AdSense from '@/components/AdSense'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -34,6 +35,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${nunito.variable}`}>
+      <GoogleAnalytics gaId="G-1QJZ3MMDSE" />
+      <GoogleTagManager gtmId="GTM-W8RT7DHD" />
       <AdSense pId="3246541131736689" />
       <body className={poppins.className}>
         {children}
